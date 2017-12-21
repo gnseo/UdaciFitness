508916c387346dda915c62865b5228dbde751458
kieat <kieat.seo@gmail.com>
2017-12-21 22:01:55 +0800

    finished

---
+++ components/DateHeader.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import { purple } from '../utils/colors'
+
+export default function DateHeader({date}){
+    return (
+      <View>
+        <Text style={{color: purple, fontSize: 25}}>{date}</Text>
+      </View>
+    )
+
+}
